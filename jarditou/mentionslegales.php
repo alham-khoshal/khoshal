205829b75f554b823802fc6f7c78f28afe819898
80010-84-11 <80010-84-11@exchange.ad.afpanet>
2020-01-27 11:17:10 +0100

    commit initial

---
+++ jarditou/mentionslegales.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+<head>
+	<?php include("head.html");?>
+
+
+</head>
+<body>
+	<?php include("contact.html");?>
+	<?php include("tableua.html");?>
+	<?php include("accueil.html");?>
+	<?php include("mentionslegales.html");?>
+    <?php include("horaires.html");?>
+    <?php include("plandesite.html");?>
+</body>
+</html>
